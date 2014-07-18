5ebb68218315c62a2dac4bdb8f5cd6cb1827f538
logsys11 <transpois0n@me.com>
2014-07-18 14:29:09 +0100

    Attempt to fix event not being transmitted

--- LogOS Files/LogOS/DAPIs/tasking.lua
+++ LogOS Files/LogOS/DAPIs/tasking.lua
@@ -11,23 +11,26 @@ function runProc(proc)
 	x,y = term.getSize()
 	nWindow = window.create(term.native(),1,2,x,y-1,true)
 	term.redirect(nWindow)
+	event, arg1 = coroutine.resume(proc.coroutine)
 	while true do
-		event,arg1, arg2, arg3, arg4, arg5, arg6 = coroutine.resume(proc.coroutine)
 		if coroutine.status(proc.coroutine) == "dead" then
-			if not event then print(arg1) sleep(1.2) end
+			if not event then term.setBackgroundColor(colors.black) print(arg1) sleep(1.2) end
 			proc = nil
 			term.redirect(term.native())
 			nWindow.setVisible(false)
 			nWindow = nil
-			return "Terminated"
+			break
 		end
-		event, arg1, arg2, arg3, arg4, arg5, arg6 = os.pullEventRaw(event)
+		event, arg1, arg2, arg3, arg4, arg5, arg6 = os.pullEvent(event)
 		if event == "mouse_click" then
 			if arg3 == "-1" then
 				--getStatusBar()
 				--Terminate for now
-				proc = nil term.redirect(term.native()) nWindow.setVisible(false) nWindow = nil return "Terminated"
+				proc = nil term.redirect(term.native()) nWindow.setVisible(false) nWindow = nil break
 			end
+
 		end
+		event, arg1 = coroutine.resume(proc.coroutine, arg1,arg2,arg3,arg4,arg5,arg6)
 	end
+	return nil
 end
